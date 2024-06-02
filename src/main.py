7223040cd796dd57a39d70c96f21a532b3688388
czeiter <czeiter@ethz.ch>
2024-06-02 12:00:33 +0200

    added a config file and made __main__ condition to the file

--- src/main.py
+++ src/main.py
@@ -4,12 +4,24 @@ from population import initializePopulation, updateUrgencyAndVOT
 from models import travel_time_model_random, expected_cost_model
 from plotting import plot_results
 import random
+import json
+import os
 
 def set_seed(seed:int) -> None:
     """Set random seed (reproducibility)"""
     np.random.seed(seed)
     random.seed(seed)
 
+def load_parameters_json(filename: str) -> dict:
+    """Open the path to a json file and return it's content"""
+    path_file = os.path.dirname(__file__) + "/"+ filename
+
+    # check if file exists
+    if not os.path.exists(path_file):
+        raise FileNotFoundError(f"The path '{path_file}' is missing. Must be in same folder as main")
+    
+    with open(path_file, "r") as read_file:
+        return json.load(read_file)
 
 def expected_time_model(route : int, memory_routeA: list, memory_routeB :list) -> float:
     """Calculate the expected travel time for a given route."""
@@ -38,8 +50,7 @@ def expected_time_model(route : int, memory_routeA: list, memory_routeB :list) -
             else:
                 return memory_routeB[0]
 
-
-def makeDecision(urgency: int, salary: int, memory_routeA: list, memory_routeB: list, history_reportedA : list, history_reportedB :list) -> int:
+def makeDecision(exploration_rate: int, urgency: int, salary: int, memory_routeA: list, memory_routeB: list, history_reportedA : list, history_reportedB :list) -> int:
     """ Return which route to take (0 or 1) based on multiple parameters"""
 
     # Check inputs for validity
@@ -55,7 +66,7 @@ def makeDecision(urgency: int, salary: int, memory_routeA: list, memory_routeB: 
     if expected_time_A==-1 or expected_time_B==-1:  #What happens here??? - czeiter
         return np.random.randint(2)
     
-    
+
     # Calculate the expected costs for each route
     expected_cost_A = expected_cost_model(route=0, exp_time=expected_time_A, pers_salary=salary, pers_urgency=urgency)
     expected_cost_B = expected_cost_model(route=1, exp_time=expected_time_B, pers_salary=salary, pers_urgency=urgency)
@@ -77,80 +88,79 @@ def makeDecision(urgency: int, salary: int, memory_routeA: list, memory_routeB: 
         return np.random.randint(2)
 
 
+def run_simulation(simulation_time: int, pop_size: int, urgency_scenario: int, history_length_personal: int, history_length_reported: int, exploration_rate: float, system_optimum: int, nash_equilibrium: int) -> None:
+    """Run the entire simulation"""
+    set_seed(42)
 
-# Parameters
-simulation_time = 100
-pop_size = 10000
-urgency_scenario = 0
-history_length_personal = 5
-history_length_reported = 5
-exploration_rate = 0.05
+    # Initialize Population
+    population = initializePopulation(pop_size, urgency_scenario)
 
-system_optimum = 3983 
-nash_equilibrium = 6169
+    # Initialize Population Memory
+    memory_route_A = [[] for i in range(0, len(population))]
+    memory_route_B = [[] for i in range(0, len(population))]
+    history_reported_A = []
+    history_reported_B = []
 
-set_seed(42)
+    # Init Recording
+    df_records = pd.DataFrame([], columns=["time", "flow_A", "flow_B", "mean_traveltime_A", "mean_traveltime_B"])
 
-# Initialize Population
-population = initializePopulation(pop_size, urgency_scenario)
-
-# Initialize Population Memory
-memory_route_A = [[] for i in range(0, len(population))]
-memory_route_B = [[] for i in range(0, len(population))]
-history_reported_A = []
-history_reported_B = []
-
-# Init Recording
-df_records = pd.DataFrame([], columns=["time", "flow_A", "flow_B", "mean_traveltime_A", "mean_traveltime_B"])
-
-# Iterate Over Time
-for time in range(0, simulation_time):
-    # make decisions
-    decisions = np.asarray([makeDecision(
-            urgency = population["urgency"].iloc[idx],
-            salary = population["salary"].iloc[idx],
-            memory_routeA = memory_route_A[idx], 
-            memory_routeB = memory_route_B[idx], 
-            history_reportedA = history_reported_A, 
-            history_reportedB = history_reported_B
-        ) for idx in range(0, len(population))])
     
-    # generate travel times
-    flow_A = len(decisions) - sum(decisions)
-    flow_B = sum(decisions)
-    travel_times = np.asarray([travel_time_model_random(route, flow_A, flow_B) for route in decisions])
-    mean_traveltime_A = np.mean(travel_times[decisions==0])
-    mean_traveltime_B = np.mean(travel_times[decisions==1])
-    
-    # update histories
-        # update reported history
-    history_reported_A.append(mean_traveltime_A)
-    history_reported_B.append(mean_traveltime_B)
-        # Remove oldest time in the reported memory
-    if len(history_reported_A) > history_length_reported:
-        history_reported_A.pop(0)
-    if len(history_reported_B) > history_length_reported:
-        history_reported_B.pop(0)
-        # update memory history for each individual
-    for idx in range(0, len(decisions)):
-        decision = decisions[idx]
-        if decision==0:
-            memory_route_A[idx].append(travel_times[idx])
-            # removes oldest memory, after enough time
-            if len(memory_route_A[idx]) > history_length_personal:
-                memory_route_A[idx].pop(0)
-        else:
-            memory_route_B[idx].append(travel_times[idx])
-            # removes oldest memory, after enough time
-            if len(memory_route_B[idx]) > history_length_personal:
-                memory_route_B[idx].pop(0)
+    # Iterate Over Time
+    for time in range(0, simulation_time):
+        # make decisions
+        decisions = np.asarray([makeDecision(
+                exploration_rate = exploration_rate,
+                urgency = population["urgency"].iloc[idx],
+                salary = population["salary"].iloc[idx],
+                memory_routeA = memory_route_A[idx], 
+                memory_routeB = memory_route_B[idx], 
+                history_reportedA = history_reported_A, 
+                history_reportedB = history_reported_B
+            ) for idx in range(0, len(population))])
         
-    # recording
-    df_records.loc[len(df_records.index)] = [time, flow_A, flow_B, mean_traveltime_A, mean_traveltime_B] 
+        # generate travel times
+        flow_A = len(decisions) - sum(decisions)
+        flow_B = sum(decisions)
+        travel_times = np.asarray([travel_time_model_random(route, flow_A, flow_B) for route in decisions])
+        mean_traveltime_A = np.mean(travel_times[decisions==0])
+        mean_traveltime_B = np.mean(travel_times[decisions==1])
         
-    # update urgencies randomly
-    population = updateUrgencyAndVOT(population, urgency_scenario)
+        # update histories
+            # update reported history
+        history_reported_A.append(mean_traveltime_A)
+        history_reported_B.append(mean_traveltime_B)
+            # Remove oldest time in the reported memory
+        if len(history_reported_A) > history_length_reported:
+            history_reported_A.pop(0)
+        if len(history_reported_B) > history_length_reported:
+            history_reported_B.pop(0)
+            # update memory history for each individual
+        for idx in range(0, len(decisions)):
+            decision = decisions[idx]
+            if decision==0:
+                memory_route_A[idx].append(travel_times[idx])
+                # removes oldest memory, after enough time
+                if len(memory_route_A[idx]) > history_length_personal:
+                    memory_route_A[idx].pop(0)
+            else:
+                memory_route_B[idx].append(travel_times[idx])
+                # removes oldest memory, after enough time
+                if len(memory_route_B[idx]) > history_length_personal:
+                    memory_route_B[idx].pop(0)
+            
+        # recording
+        df_records.loc[len(df_records.index)] = [time, flow_A, flow_B, mean_traveltime_A, mean_traveltime_B] 
+            
+        # update urgencies randomly
+        population = updateUrgencyAndVOT(population, urgency_scenario)
+        
+        print(f"Time step {time + 1}/{simulation_time} completed")
     
-    print(time, "/", simulation_time)
-    
-plot_results(df_records, system_optimum, nash_equilibrium)
+    plot_results(df_records, system_optimum, nash_equilibrium)
+
+
+if __name__ == "__main__":
+    # Parameters
+    config = load_parameters_json("config.json")
+
+    run_simulation(simulation_time=config["simulation_time"], pop_size=config['pop_size'], urgency_scenario=config['urgency_scenario'], history_length_personal=config['history_length_personal'], history_length_reported=config['history_length_reported'], exploration_rate=config['exploration_rate'], system_optimum=config['system_optimum'], nash_equilibrium=config['nash_equilibrium'])
