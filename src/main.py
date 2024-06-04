ffd7b28efe9d3f2d550301b8e68f38b096622afa
czeiter <czeiter@ethz.ch>
2024-06-04 15:40:49 +0200

    undo all optimization changes

--- src/main.py
+++ src/main.py
@@ -6,8 +6,6 @@ from decision import makeDecision
 from plotting import plot_results
 from helpers import set_seed, load_parameters_json, csv_results, json_output
 from datetime import datetime
-from collections import deque
-
 
 def run_simulation(simulation_time: int, pop_size: int, urgency_scenario: int, history_length_personal: int, history_length_reported: int, exploration_rate: float, system_optimum: int, nash_equilibrium: int, simulation_type : int, history_weight_personal:float, history_weight_reported:float,  config) -> None:
     """Run the entire simulation"""
@@ -29,13 +27,13 @@ def run_simulation(simulation_time: int, pop_size: int, urgency_scenario: int, h
     population = initializePopulation(pop_size, urgency_scenario)
 
     # Initialize Population Memory
-    memory_route_A = [deque(maxlen=history_length_personal) for _ in range(len(population))]
-    memory_route_B = [deque(maxlen=history_length_personal) for _ in range(len(population))]
-    history_reported_A = deque(maxlen=history_length_reported)
-    history_reported_B = deque(maxlen=history_length_reported)
+    memory_route_A = [[] for i in range(0, len(population))]
+    memory_route_B = [[] for i in range(0, len(population))]
+    history_reported_A = []
+    history_reported_B = []
 
     # Init Recording
-    records = [None] * simulation_time
+    df_records = pd.DataFrame([], columns=["time", "flow_A", "flow_B", "mean_traveltime_A", "mean_traveltime_B"])
 
     
     # Iterate Over Time
@@ -54,49 +52,44 @@ def run_simulation(simulation_time: int, pop_size: int, urgency_scenario: int, h
                 history_weight_reported = history_weight_reported
             ) for idx in range(0, len(population))])
         
-        
         # generate travel times
-        num_decisions = len(decisions)
-        sum_decisions = sum(decisions)
-        flow_A = num_decisions - sum_decisions
-        flow_B = sum_decisions
-        travel_times = travel_time_model_random(decisions, flow_A, flow_B)
+        flow_A = len(decisions) - sum(decisions)
+        flow_B = sum(decisions)
+        travel_times = np.asarray([travel_time_model_random(route, flow_A, flow_B) for route in decisions])
         mean_traveltime_A = np.mean(travel_times[decisions==0])
         mean_traveltime_B = np.mean(travel_times[decisions==1])
         
         # update histories
             # update reported history
-        history_reported_A.append(mean_traveltime_A) #these are deques, so if max_size is reached, it drops the first values
+        history_reported_A.append(mean_traveltime_A)
         history_reported_B.append(mean_traveltime_B)
-
+            # Remove oldest time in the reported memory
+        if len(history_reported_A) > history_length_reported:
+            history_reported_A.pop(0)
+        if len(history_reported_B) > history_length_reported:
+            history_reported_B.pop(0)
             # update memory history for each individual
-        for idx in range(0, num_decisions):
+        for idx in range(0, len(decisions)):
             decision = decisions[idx]
             if decision==0:
-                memory_route_A[idx].append(travel_times[idx]) #these are deques, so if max_size is reached, it drops the first values
+                memory_route_A[idx].append(travel_times[idx])
+                # removes oldest memory, after enough time
+                if len(memory_route_A[idx]) > history_length_personal:
+                    memory_route_A[idx].pop(0)
             else:
                 memory_route_B[idx].append(travel_times[idx])
+                # removes oldest memory, after enough time
+                if len(memory_route_B[idx]) > history_length_personal:
+                    memory_route_B[idx].pop(0)
             
         # recording
-
-        records.append({
-            "time": time,
-            "flow_A": flow_A,
-            "flow_B": flow_B,
-            "mean_traveltime_A": mean_traveltime_A,
-            "mean_traveltime_B": mean_traveltime_B
-        })
-
+        df_records.loc[len(df_records.index)] = [time, flow_A, flow_B, mean_traveltime_A, mean_traveltime_B] 
             
         # update urgencies randomly
         population = updateUrgencyAndVOT(population, urgency_scenario)
         
         print(f"Time step {time + 1}/{simulation_time} completed")
     
-
-    # Create dataframe (done so late, because lists are more efficient than dataframe)
-    df_records = pd.DataFrame(records)
-
     current_time = datetime.now().strftime("%Y%m%d_%H%M%S")
     plot_results(df_records, system_optimum, nash_equilibrium, config, current_time)
     csv_results(df_records, current_time)
